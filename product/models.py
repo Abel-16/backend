3884dd6b07e80cb3bfa9a50204bfd9eeb68b5eba
Abel <se.abel.tadesse@gmail.com>
2024-03-30 16:38:04 +0300

    working on the product model

--- product/models.py
+++ product/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now = True)
+    created_date = models.DateTimeField(auto_now = True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT)
+    
+    def __str__(self) -> str:
+        return self.title
+    
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Product', on_delete = models.SET_NULL, null = True, related_name = '+')
+    
+    def __str__(self) -> str:
+        return self.title
