3f53e76ccf0bf31fee5d52e6520dfa7ed5f1cbef
Abel <se.abel.tadesse@gmail.com>
2024-04-02 22:23:30 +0300

    adding swagger view and making it home url

--- backend/urls.py
+++ backend/urls.py
@@ -2,10 +2,13 @@
 import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
+from .swagger import schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('__debug__/', include(debug_toolbar.urls)),
     path('test/', include('testingapp.urls')),
 ]
