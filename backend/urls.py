b768a09400b34798da09c4009c27d1fc6e325727
Abel <se.abel.tadesse@gmail.com>
2024-04-08 20:14:22 +0300

    modifying the url patterns

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,13 @@
-
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import permissions
+from .swagger import schema_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('authentication.urls')),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
