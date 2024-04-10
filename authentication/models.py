d49dc1cc16d50020bfc5d13e7a1b9c458d89ce96
Abel <se.abel.tadesse@gmail.com>
2024-04-10 13:53:31 +0300

    adding phone to the model

--- authentication/models.py
+++ authentication/models.py
@@ -1,26 +1,29 @@
 from django.db import models
 from django .contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
-
+from django.core.validators import RegexValidator
 
 from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class UserManager(BaseUserManager):
     
-    def create_user(self, username, email, password=None):
+    def create_user(self, username, email, phone_number, password=None):
         
         if username is None:
-            raise TypeError("Users should have a username")
+            raise TypeError("Users should have a Username")
         
         if email is None:
-            raise TypeError("Users should have an email")
+            raise TypeError("Users should have an Email")
         
-        user = self.model(username=username, email=self.normalize_email(email))
+        if phone_number is None:
+            raise TypeError("Users should have an Phone Number")
+        
+        user = self.model(username=username, email=self.normalize_email(email), phone_number = phone_number)
         user.set_password(password)
         user.save()
         return user
     
-    def create_superuser(self, username, email, password):
+    def create_superuser(self, username, email, phone_number, password):
         
         if password is None:
             raise TypeError("Password should not be none")
@@ -35,6 +38,8 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length = 255, unique = True, db_index = True)
     email = models.EmailField(max_length=254, unique= True, db_index = True)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) # Validators should be a list
     is_verified = models.BooleanField(default = False)
     is_active = models.BooleanField(default = True)
     is_staff = models.BooleanField(default=False)
