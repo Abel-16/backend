7af558f99ffc820ed69b6245b44cf281aac3ec3b
Abel <se.abel.tadesse@gmail.com>
2024-04-10 13:46:29 +0300

    adding api to urls prefixes

--- backend/urls.py
+++ backend/urls.py
@@ -5,9 +5,9 @@ from .swagger import schema_view
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('auth/', include('authentication.urls')),
-    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/admin/', admin.site.urls),
+    path('api/auth/', include('authentication.urls')),
+    path('api/swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('api/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
