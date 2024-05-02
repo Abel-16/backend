cc0a619c4846ab40e3ab8e35863a49390e58dab1
Abel <se.abel.tadesse@gmail.com>
2024-05-02 13:29:02 +0300

    store apis done

--- backend/urls.py
+++ backend/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('api/admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
     path('api/expanses/', include('expenses.urls')),
+    path('api/store/', include('store.urls')),
     path('api/swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('api/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
