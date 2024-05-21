1c0f747bc1c54ea6e031ccec1e8130a4feb07e0a
Abel <se.abel.tadesse@gmail.com>
2024-05-21 15:00:06 +0300

    payment chapa

--- payment/urls.py
+++ payment/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('initiate/', views.initiate_payment, name='initiate_payment'),
-    path('callback/', views.payment_callback, name='payment_callback'),
+    path('payment/', views.PaymentAPIView.as_view(), name='initiate_payment'),
+    # path('callback/', views.payment_callback, name='payment_callback'),
 ]
