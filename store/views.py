74ef46e0ebff934fbe9931b1e0b5bc3627e8ddee
Abel <se.abel.tadesse@gmail.com>
2024-06-03 13:01:18 +0300

    cart detail

--- store/views.py
+++ store/views.py
@@ -114,3 +114,70 @@ class CartListView(generics.ListAPIView):
             queryset = Cart.objects.filter(cart_id=cart_id)
 
         return queryset
+
+class CartListView(generics.RetrieveAPIView):
+    serializer_class = CartSerializer
+    permission_classes = [AllowAny]
+    lookup_field = "cart_id"
+    queryset = Cart.objects.all()
+    
+    def get_queryset(self):
+        cart_id = self.kwargs.get('cart_id')
+        user_id = self.kwargs.get('user_id')
+
+        if user_id is not None:
+            user = User.objects.filter(id=user_id).first()
+            if user:
+                queryset = Cart.objects.filter(user=user, cart_id=cart_id)
+            else:
+                queryset = Cart.objects.none()  # No user found, return empty queryset
+        else:
+            queryset = Cart.objects.filter(cart_id=cart_id)
+
+        return queryset
+    
+    def get(self, reuest, *args, **kwargs):
+        queryset = self.get_queryset()
+        
+        total_shipping = 0.0
+        total_tax = 0.0
+        total_service_fee = 0.0
+        total_sub_total = 0.0
+        total_total = 0.0
+        
+        for cart_item in queryset:
+            total_shipping += self.calculate_shipping(cart_item)
+            total_tax += self.calculate_tax(cart_item)
+            total_service_fee += self.calculate_service_fee(cart_item)
+            total_sub_total += self.calculate_sub_total(cart_item)
+            total_total += self.calculate_total(cart_item)
+            
+        data = {
+            'shipping' : total_shipping,
+            'total_tax' : total_tax,
+            'total_service_fee' : total_service_fee,
+            'total_sub_total' : total_sub_total,
+            'total_total' : total_total,
+          
+        }
+        
+        return Response(data)
+    def calculate_shipping(self, cart_item):
+        return cart_item.shipping__amount
+    
+    def calculate_tax(self, cart_item):
+        return cart_item.tax_fee
+    
+    
+    def calculate_service_fee(self, cart_item):
+        return cart_item.service_fee
+    
+    
+    def calculate_sub_total(self, cart_item):
+        return cart_item.sub_total
+    
+    def calculate_total(self, cart_item):
+        return cart_item.total
+    
+    
+    
